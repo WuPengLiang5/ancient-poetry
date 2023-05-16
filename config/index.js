6a9b1fd3cd473bbf2ae9ff6780b5cb01608a1f8a
WuPengLiang5 <2523157228@qq.com>
2023-05-16 14:40:53 +0800

    修改配置文件

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/ancient-poetry/',
     proxyTable: {},
 
     // Various Dev Server settings
